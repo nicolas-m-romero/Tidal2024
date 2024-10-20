34f7c93e69c2b9ed490349117f9d9489b452492a
nicolas-m-romero <117554210+nicolas-m-romero@users.noreply.github.com>
2024-10-19 22:28:19 -0500

    messing around with frontend, and declaring colors in tailwind config

--- trainr/tailwind.config.js
+++ trainr/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary100: '#C21D03',  // Define your custom colors
+        primary200: '#FD5732',
+        primary300: "#FFB787",
+        accent100: "#393939",
+        accent200: "#BEBEBE",
+        text100: "#231121",
+        text200: "#4B4848",
+        bg100: "#FBFBFB",
+        bg200: "#F1F1F1",
+        bg300: "#C8C8C8"
+      },
+    },
   },
   plugins: [],
 }
