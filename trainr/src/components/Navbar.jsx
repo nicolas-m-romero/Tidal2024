ec3089d498431486246692b08a60c03ffd557b3d
liannie3 <138341881+liannie3@users.noreply.github.com>
2024-10-20 06:21:34 -0500

    logo fix

--- trainr/src/components/Navbar.jsx
+++ trainr/src/components/Navbar.jsx
@@ -6,17 +6,20 @@ import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
-
+    const handleLogoClick = () => {
+        navigate('/'); // Navigate to the landing page
+    };
     return (
         <nav className="font-h1 flex justify-between items-center py-4 px-6">
         {/* Left Side (Logo) */}
         <div className="flex items-center">
-            <img 
+                <img 
                     src={Logo} 
-                    className="w-[70px] h-auto" 
+                    className="w-[70px] h-auto cursor-pointer" // Added cursor-pointer for better UX
                     alt="Logo"
                     onMouseOver={(e) => e.currentTarget.src = LogoGif} // Change to GIF on hover
                     onMouseOut={(e) => e.currentTarget.src = Logo} // Change back to static on mouse out
+                    onClick={handleLogoClick} // Navigate on logo click
                 />
         </div>
     
