af3eb53ff93c9cfe305d913b5cb63b8615af72a4
liannie3 <138341881+liannie3@users.noreply.github.com>
2024-10-20 06:05:22 -0500

    Added logo hover anim + fixed react import

--- trainr/src/components/Navbar.jsx
+++ trainr/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './Button'
 import Logo from '../assets/logo.png';
+import LogoGif from '../assets/logogif.gif'
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
@@ -10,13 +11,13 @@ const Navbar = () => {
         <nav className="font-h1 flex justify-between items-center py-4 px-6">
         {/* Left Side (Logo) */}
         <div className="flex items-center">
-            <a onClick={() => navigate('/')}>
             <img 
-                src={Logo} 
-                className="w-[70px] h-auto" 
-                alt="Logo"
-            />
-            </a>
+                    src={Logo} 
+                    className="w-[70px] h-auto" 
+                    alt="Logo"
+                    onMouseOver={(e) => e.currentTarget.src = LogoGif} // Change to GIF on hover
+                    onMouseOut={(e) => e.currentTarget.src = Logo} // Change back to static on mouse out
+                />
         </div>
     
         {/* Right Side (Buttons) */}
